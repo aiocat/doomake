50c27556d6547ec88cce2c975c07ee0c4ad25139
aiocat <aiocat@protonmail.com>
2022-03-08 20:11:16 +0300

    add run command(s)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Doomake
-Simple Makefile (like) clone written in C.
+Simple <50 line Makefile (like) clone written in C.
+
+## Limitations
+- Line length must be less than 1024 byte (1kb)
--- doomake.c
+++ doomake.c
@@ -4,8 +4,7 @@
 
 int main(int argc, char **argv)
 {
-    if (argc < 2)
-    { // check argument size
+    if (argc < 2) { // check argument size
         puts("Command name must be given!");
         return EXIT_FAILURE;
     }
@@ -13,28 +12,38 @@ int main(int argc, char **argv)
     const char *command = argv[1]; // get first argument
 
     FILE *filePointer;
-    char line[256];
+    char line[1025];
 
     filePointer = fopen("Doomake", "r"); // open doomake file in read mode
-    if (filePointer == NULL)
-    { // check if file exists
+    if (filePointer == NULL) { // check if file exists
         puts("Can't find Doomake file");
-        exit(EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
 
     char commandName[256]; // command name format
     strcpy(commandName, command);
     strcat(commandName, ":\n");
 
-    while (fgets(line, sizeof(line), filePointer))
-    {
-        if (strcmp(line, commandName) == 0)
-        {
-            puts("Got it!");
-        }
+    int collect = 0; // collect value
 
+    while (fgets(line, sizeof(line), filePointer)) // read line-by-line
+    {
+        if (collect) // if collect
+        {
+            if (strcmp(line, "\n") == 0) break; // stop collecting if new line
+            else system(line); // run if not only new line
+        }
+        else {
+            if (strcmp(line, commandName) == 0) collect = 1; // if command name matches
+        }
     }
 
-    fclose(filePointer);
+    fclose(filePointer); // close file
+
+    if (collect == 0) { // check if collected a command
+        puts("Command not found");
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
