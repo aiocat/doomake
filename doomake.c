4224d1c78ec82330615ddea091dc27f4beef8833
aiocat <aiocat@protonmail.com>
2022-03-08 23:00:03 +0300

    minify code

--- doomake.c
+++ doomake.c
@@ -18,46 +18,34 @@
 #include <unistd.h>
 
 // write error to stderr
-int write_error(char* msg) {
-    fprintf(stderr, msg);
-    return EXIT_FAILURE;
+int write_error(char* msg) { 
+    fprintf(stderr, msg); 
+    return EXIT_FAILURE; 
 }
 
-int main(int argc, char **argv)
-{
-    if (argc < 2) { // check argument size
-        return write_error("Command name must be given!");
-    }
+int main(int argc, char **argv) {
+    if (argc < 2) return write_error("Command name must be given!"); // check argument size
 
-    const char *command = argv[1]; // get first argument
-    char line[1025]; // line buffer
+    int collect = 0; // collect value
+    const char* command = argv[1]; // get first argument
+    char line[1025], commandName[256]; // line buffer and command name format
     FILE *filePointer; // file pointer
 
     filePointer = fopen("Doomake", "r"); // open doomake file in read mode
-    if (filePointer == NULL) { // check if file exists
-        return write_error("Can't find Doomake file");
-    }
-
-    char commandName[256]; // command name format
-    int collect = 0; // collect value
+    if (filePointer == NULL) return write_error("Can't find Doomake file"); // check if file exists
 
     strcpy(commandName, command);
     strcat(commandName, ":\n");
 
-    while (fgets(line, sizeof(line), filePointer)) // read line-by-line
-    {
-        if (collect) // if collect
-        {
+    while (fgets(line, sizeof(line), filePointer)) { // read line-by-line
+        if (collect) { // if collect
             if (strcmp(line, "\n") == 0) break; // stop collecting if new line
             else system(line); // run if not only new line
-        }
-        else if (strcmp(line, commandName) == 0) collect = 1; // if command name matches
+        } else if (strcmp(line, commandName) == 0) collect = 1; // if command name matches
     }
 
     fclose(filePointer); // close file
-    if (collect == 0) { // check if collected a command
-        return write_error("Command not found");
-    }
+    if (collect == 0) return write_error("Command not found"); // check if collected a command
 
     return EXIT_SUCCESS;
 }
