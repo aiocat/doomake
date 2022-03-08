ff19f331be67cf135c6bd997e9fceff539fe381a
aiocat <aiocat@protonmail.com>
2022-03-08 22:10:45 +0300

    update error messages

--- doomake.c
+++ doomake.c
@@ -1,30 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+
+// write error to stderr
+int write_error(char* msg) {
+    fprintf(stderr, msg);
+    return EXIT_FAILURE;
+}
 
 int main(int argc, char **argv)
 {
     if (argc < 2) { // check argument size
-        puts("Command name must be given!");
-        return EXIT_FAILURE;
+        return write_error("Command name must be given!");
     }
 
     const char *command = argv[1]; // get first argument
-
-    FILE *filePointer;
-    char line[1025];
+    FILE *filePointer; // file pointer
+    char line[1025]; // line buffer
 
     filePointer = fopen("Doomake", "r"); // open doomake file in read mode
     if (filePointer == NULL) { // check if file exists
-        puts("Can't find Doomake file");
-        return EXIT_FAILURE;
+        return write_error("Can't find Doomake file");
     }
 
     char commandName[256]; // command name format
+    int collect = 0; // collect value
+
     strcpy(commandName, command);
     strcat(commandName, ":\n");
-
-    int collect = 0; // collect value
 
     while (fgets(line, sizeof(line), filePointer)) // read line-by-line
     {
@@ -33,16 +37,12 @@ int main(int argc, char **argv)
             if (strcmp(line, "\n") == 0) break; // stop collecting if new line
             else system(line); // run if not only new line
         }
-        else {
-            if (strcmp(line, commandName) == 0) collect = 1; // if command name matches
-        }
+        else if (strcmp(line, commandName) == 0) collect = 1; // if command name matches
     }
 
     fclose(filePointer); // close file
-
     if (collect == 0) { // check if collected a command
-        puts("Command not found");
-        return EXIT_FAILURE;
+        return write_error("Command not found");
     }
 
     return EXIT_SUCCESS;
